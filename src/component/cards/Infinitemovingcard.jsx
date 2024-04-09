2e01b271aa233ba57c615d8ad1deaaa669d2c35f
bitbard3 <ansharora3839@gmail.com>
2024-04-09 11:03:05 +0530

    Change card style

--- src/component/cards/Infinitemovingcard.jsx
+++ src/component/cards/Infinitemovingcard.jsx
@@ -6,23 +6,29 @@ function Infinitemovingcard() {
   return (
     <div>
       <Marquee>
-        <div className="flex m-2">
+        <div className="flex m-2 ">
           {cardsData.map((card, index) => (
             <div
               key={index}
-              className="w-[220px] h-[350px] grid border border-solid border-gray-400 mt-[20px] rounded-lg "
+              className="w-[280px] h-[350px] flex flex-col items-center border card-shadow border-dark mt-[20px] rounded-lg mx-3 px-3"
             >
-              <div className=" h-[130px]">
+              <div className="flex w-full justify-between items-center mt-3 mb-1.5">
+                <div className="px-5 py-0.5 rounded-lg bg-main flex items-center justify-center">
+                  <p className="text-white text-lg mt-1">{card.views}</p>
+                </div>
+                <div className="px-5 py-0.5 rounded-lg bg-red-500 flex items-center justify-center">
+                  <p className="text-white text-lg mt-1">Live</p>
+                </div>
+              </div>
+              <div className=" w-[95%] flex justify-center">
                 <img
                   src={card.image}
-                  className="h-[180px] w-[200px] m-2 rounded-xl bg-black"
+                  className="h-[180px] w-full m-2 object-cover rounded-xl bg-black"
                 />
               </div>
-              <div className="text-black font-bold m-1">
-                <p>Name: {card.name}</p>
-                <p>Age: {card.age}</p>
-                <p>City: {card.city}</p>
-                <p className="text-red-600 font-bold m-1  text-[20px]">Live.</p>
+              <div className="mr-auto ml-2 mt-5">
+                <p className="text-dark text-2xl"><span className="text-neutral-500 mr-2">Name</span> <span className="font-medium">{card.name}</span></p>
+                <p className="text-dark text-2xl mt-1"><span className="text-neutral-500 mr-6">City</span> <span className="ml-1 font-medium">{card.city}</span></p>
               </div>
             </div>
           ))}
